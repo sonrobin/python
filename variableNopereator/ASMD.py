e6ea7c0067709e24fa84299a7df4c85876daaefc
sonrobin <36331157+sonrobin@users.noreply.github.com>
2021-03-12 16:07:47 +0900

    Add files via upload

---
+++ variableNopereator/ASMD.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# 두 정수를 입력받아 사직연산 결과를 출력
+
+a = int(input("첫 번째 숫자 : "))
+b = int(input("두 번째 숫자 : "))
+
+print(a,"+",b,"=",a+b)
+print(a,"-",b,"=",a-b)
+print(a,"*",b,"=",a*b)
+print(a,"/",b,"=",a/b)
+
+
+'''
+블록 주석
+a = int(input("첫 번째 숫자 : "))
+b = int(input("두 번째 숫자 : "))
+
+print(a,"+",b,"=",a+b)
+print(a,"-",b,"=",a-b)
+print(a,"*",b,"=",a*b)
+print(a,"/",b,"=",a/b)
+'''

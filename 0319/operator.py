26c4b4a1aee401d490b86ebbbad4313c6e204586
sonrobin <36331157+sonrobin@users.noreply.github.com>
2021-03-24 10:54:49 +0900

    Add files via upload

---
+++ 0319/operator.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 19 15:33:44 2021
+
+@author: User
+"""
+
+a = 3
+b = ~a
+
+print("a =", a,"b =",b, "b =  b + 1 " , (b+1))
+
+a = 3
+b = 5
+c = a&b
+
+print("a =",a,"b =",b, "c =", c)
+
+a = 3
+b = 5
+c = a^b
+
+print("a =", a,"b =",b, "c =", c)
+
+a = 3
+b = 5
+c = a|b
+
+print("a =", a,"b =",b, "c =", c)

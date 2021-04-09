2e6fe93ee43456d258302961f2599b620947ad41
sonrobin <36331157+sonrobin@users.noreply.github.com>
2021-04-09 15:51:10 +0900

    Update listEx.py

--- 0409/listEx.py
+++ 0409/listEx.py
@@ -11,7 +11,7 @@ stdNum = [2,0,2,1,0,6,0,3,6]
 
 print(fstName)
 print(sndName)
-# 화면에 s,o,n 을 출렭하는 프로그램을 작성하시오.
+# 화면에 S,R,B 을 출렭하는 프로그램을 작성하시오.
 print(fstName[0])   # S
 print(sndName[0][0])   # R
 print(sndName[1][0])   # B
